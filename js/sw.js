2b9aed69db92f4df3b112260ea481414860161f2
Saung Park <spark89@gmail.com>
2018-06-18 20:52:07 -0400

    made URLS_TO_CACHE constant

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,21 @@
 console.log('self is', self);
 
-self.addEventListener('install', e => {
-    
-});
+const URLS_TO_CACHE = [
+  'js/dbhelper.js',
+  'js/restaurant_info.js',
+  'js/main.js',
+  'css/styles.css',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg'
+];
+
+self.addEventListener('install', e => {});
